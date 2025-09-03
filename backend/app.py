6d7908a5366ae39e58fea5eac1e8be04ba335eb7
Maya OH <mayaoh2000@gmail.com>
2025-09-03 15:38:46 -0400

    Flask app generate html template

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def home():
-    return "<p>Hello, World! </p>"
+    return send_from_directory('../frontend', 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
